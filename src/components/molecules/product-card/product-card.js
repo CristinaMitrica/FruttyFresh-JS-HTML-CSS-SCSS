9def7b36d8453020203231618e10950e59094a3b
Cris <cristinamitrica9@gmail.com>
2023-07-30 22:25:50 +0200

    update

--- src/components/molecules/product-card/product-card.js
+++ src/components/molecules/product-card/product-card.js
@@ -6,14 +6,14 @@ export default class ProductCardMolecule {
 
     renderComponentHTML() {
         return `
-            <div class="product-card">
+            <article class="product-card">
                 <div class="product-card__img-frame">
                     <img class="product-card__img" src="assets/images/${this._name}.jpg" alt="Producto ${this._name} imagen" loading="lazy">
                 </div>
                 <h3 class="font--h3">${this._name}</h3>
                 <p>${this._price}€</p>
                 <button class="product-card__btn btn--filled" type="button">Añadir a la cesta</button>
-            </div>
+            </article>
         `
     }
 }
